88705c06547a596567bbb827c5f80a007e3ae5f1
Nick Gray <nicholasnbg@gmail.com>
2018-12-05 21:33:34 +1100

    day1 pt 2 solution

--- day-1/lib/freq_change.rb
+++ day-1/lib/freq_change.rb
@@ -1,5 +1,22 @@
 class FrequencyChanger
-  def self.new_freq(freqs_arr)
+  def new_freq(freqs_arr)
     freqs_arr.sum()
   end
+
+  def first_repeat(freqs_arr)
+    sum = 0
+    sums = Set.new
+
+    while true
+      freqs_arr.each do |change|
+        sum += change.to_i
+
+        if sums.include?(sum)
+          return sum
+          exit
+        end
+      sums.add(sum)
+    end
+  end
+end
 end
